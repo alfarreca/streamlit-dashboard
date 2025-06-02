02d4fabff17ebfb1fbdce1b2d08d0f96c54a0b5a
alfarreca <alfarrecas@proton.me>
2025-06-02 11:25:20 +0100

    Update ISHARES_Europe_Defence.py

--- ISHARES_Europe_Defence.py
+++ ISHARES_Europe_Defence.py
@@ -37,7 +37,7 @@ def get_google_sheet_data():
     SERVICE_ACCOUNT_INFO = st.secrets["GCP_SERVICE_ACCOUNT"]
     creds = Credentials.from_service_account_info(SERVICE_ACCOUNT_INFO, scopes=SCOPES)
     gc = gspread.authorize(creds)
-    sheet = gc.open_by_key("1c6bgowxaAcKcG3mlydh36j9TXFlFyU9GJ0CipPnOfSk").sheet1
+    sheet = gc.open_by_key("1rJ5YsfKMAcU9IItWZIvrivQbMpG6982Yh1F93YhW-Jk").sheet1
     df = pd.DataFrame(sheet.get_all_records()).dropna(subset=["Symbol", "Exchange"]).drop_duplicates("Symbol")
     return df
 
